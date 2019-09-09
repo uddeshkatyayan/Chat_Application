aa621d330e76e901cbb710fd57a44a8a6620c1d7
uddeshkatyayan <uddeshkatyayan@gmail.com>
2019-09-09 22:19:32 +0530

    bash script added

---
+++ chat_app_client.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Bash Menu Script Example
+
+PS3='Please enter your choice: '
+echo "Arg0 : $0"
+echo "Arg1 : $1"
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "you chose choice 1"
+            cd ass2_withoutEncrypt
+            javac client.java
+            java TCPClient $1 $2
+            cd ..
+            break
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            cd ass2_withoutSignature
+            javac client.java
+            java TCPClient $1 $2
+            cd ..
+            break
+            ;;
+        "Option 3")
+            echo "you chose choice $REPLY which is $opt"
+            cd ass2_withSignature
+            javac client.java
+            java TCPClient $1 $2 
+            cd ..
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
